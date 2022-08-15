5517fb2d03053c548b1e0da1762b02b61ec7fd42
Brian Jun <bmtef.02@gmail.com>
2022-08-15 15:58:56 -0700

    maintenance branch setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
-import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <html>Our site is down for maintenance</html>
 );
